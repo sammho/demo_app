abd02def843c703f4eaead46a8953fd83d4ad6d3
Sam Ho <samho@peoplematch.me>
2011-10-08 15:54:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '26e483c189fa64d8c86bd0681431dbdd5b87815d5bce4c519a3a82c03bd39bbb3cbf9cbc368135a7194e42685e00c4bf52e36c6fa90722dd3c2d0665de794258'
